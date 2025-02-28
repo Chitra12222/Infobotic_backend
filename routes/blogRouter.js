230f648437778d3cbc5e4c9016143e2dabd9674d
Chitra12222 <chitraashrivastavaa@gmail.com>
2025-02-28 21:03:20 +0530

    Initial commit

---
+++ routes/blogRouter.js
@@ -0,0 +1,23 @@
+const express=require('express')
+const router=express.Router()
+const {postBlog, otherBlog,currentUserBlogs, deleteBlog,entertainment, sports, technology, breakingnews,getBlogByid}=require('../controllers/blogController')
+const { isAuthenticated } = require('../middleware/auth')
+
+router.post('/post-blog',isAuthenticated,postBlog)
+
+router.get('/other-blog',isAuthenticated, otherBlog)
+
+router.get('/entertainment-blog',isAuthenticated, entertainment)
+
+router.get('/sports-blog',isAuthenticated, sports)
+
+router.get('/technology-blog',isAuthenticated, technology)
+
+router.get('/breakingnews-blog',isAuthenticated, breakingnews)
+
+router.post('/getcurrentuserblogs',isAuthenticated,currentUserBlogs)
+
+router.delete('/deleteblog/:id',isAuthenticated,deleteBlog)
+
+router.get('/blog/:id',getBlogByid )
+module.exports=router
